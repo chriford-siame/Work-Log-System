704d8507fef8753fcfbd22e566058f7cb8e14592
Chriford Siame <https://chriford@bitbucket.org>
2023-11-25 23:22:27 +0200

    create a navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import { IoIosTimer } from 'react-icons/io'
 
 export default function Navbar() {
   return (
-    <div>
-        <h1>Time</h1>
-        <button className=''>Logout</button>
+    <div className='flex justify-between items-center p-5'>
+      <h1 className='text-2xl text-black flex items-center'>
+        <IoIosTimer />
+        Time
+      </h1>
+      <button className='bg-black text-white px-4 py-1'>Logout</button>
     </div>
   )
 }
