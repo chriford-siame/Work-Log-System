3267f6cbd4e8de5dd0a4bc0283b99fd8c30e8858
Chriford Siame <https://chriford@bitbucket.org>
2023-11-25 23:21:31 +0200

    add tailwind custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,19 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        success: "#28a745",
+        danger: "#dc3545",
+        primary: "#007bff",
+        secondary: "#6c757d",
+        warning: "#ffc107",
+        dark: "#343a40",
+        info: "#17a2b8",
+        light: "#f8f9fa",
+        muted: "#a1a6ac",
+        link: "#53baca",
+        white: "#ffffff",
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
