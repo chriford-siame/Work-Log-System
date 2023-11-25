2cd1f41f5b0439a0483a3ac03e4bc926d12df669
Chriford Siame <https://chriford@bitbucket.org>
2023-11-26 00:33:41 +0200

    update the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Navbar from '@/components/Navbar'
+import { NewDialog } from '@/components/NewLog'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
+    <div className='p-5 space-y-10'>
       <Navbar />
+      <div className='px-5'>
+        <NewDialog />
+      </div>
     </div>
   )
 }
