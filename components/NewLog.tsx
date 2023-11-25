bf255f483b2a75dfb81e292ea93570920245acf0
Chriford Siame <https://chriford@bitbucket.org>
2023-11-25 23:43:30 +0200

    modify components

---
+++ components/NewLog.tsx
@@ -0,0 +1,38 @@
+// import {
+//   Dialog,
+//   DialogContent,
+//   DialogDescription,
+//   DialogFooter,
+//   DialogHeader,
+//   DialogTitle,
+//   DialogTrigger,
+// } from "@/components/ui/dialog"
+
+// export function NewDialog() {
+//   return (
+//     <Dialog>
+//       <DialogTrigger asChild>
+//         <button className="bg-primary rounded-xl">Trigger</button>
+//       </DialogTrigger>
+//       <DialogContent className="sm:max-w-[425px]">
+//         <DialogHeader>
+//           <DialogTitle>Edit profile</DialogTitle>
+//           <DialogDescription>
+//             Make changes to your profile here. Click save when you're done.
+//           </DialogDescription>
+//         </DialogHeader>
+//         <div className="grid gap-4 py-4">
+//           <div className="grid grid-cols-4 items-center gap-4">
+//             <input className="p-2 rounded-md border-secondary" type="text" placeholder="First name" id="first_name" />
+//           </div>
+//           <div className="grid grid-cols-4 items-center gap-4">
+//             <input className="p-2 rounded-md border-secondary" type="text" placeholder="Last name" id="last_name" />
+//           </div>
+//         </div>
+//         <DialogFooter>
+//           <button className="bg-success p-2 rounded-md" type="submit">Save changes</button>
+//         </DialogFooter>
+//       </DialogContent>
+//     </Dialog>
+//   )
+// }
