ce63dbdc397f0d86c12fe1a814be79fcfe91b177
Chriford Siame <https://chriford@bitbucket.org>
2023-11-30 14:27:44 +0200

    update files

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
+import Calendar from '@/components/Calendar'
+import Logs from '@/components/Logs'
 import Navbar from '@/components/Navbar'
 import { NewDialog } from '@/components/NewLog'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className='p-5 space-y-10'>
+    <div className='p-5 contrast-less space-y-10'>
       <Navbar />
-      <div className='px-5'>
-        <NewDialog />
-      </div>
+      <NewDialog />
+      <Calendar />
+      <Logs />
     </div>
   )
 }
